7061d4bafbfd50fb8776557ca756b1612f0ff97f
HT_SUN <HT_SUN@FINTEKHP15S01>
2022-07-01 09:34:09 +0800

    加入專案檔案。

---
+++ slnShoppingCa/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using slnShoppingCa.Models;
+using System.Web.Security;  //web應用程式表單驗證服務
+
+namespace slnShoppingCa.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbShoppingCarEntities1 db = new dbShoppingCarEntities1();
+        public ActionResult Index()
+        {
+            //取得所有產品放入products
+            var products = db.tProduct.OrderByDescending(m => m.fId).ToList();
+            return View(products);
+        }
+
+        //Get: Home/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Post: Home/Login
+        [HttpPost]
+        public ActionResult Login(string fUserId, string fPwd)
+        {
+            //依帳密取得會員並指定給member
+            var member = db.tMember.Where(m=>m.fUserId == fUserId && m.fPwd == fPwd).FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if(member == null)
+            {
+                ViewBag.Message = "帳密錯誤，登入失敗";
+                return View();
+            }
+            //使用session 變數記錄歡迎詞
+            Session["Welcome"] = member.fName + "歡迎光臨";
+            //指定使用者帳號通過驗證
+            FormsAuthentication.RedirectFromLoginPage(fUserId, true);
+            return RedirectToAction("Index", "Member");
+        }
+
+        //Get:Home/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //Post:Hpme/Register
+        [HttpPost]
+        public ActionResult Register(tMember pMember)
+        {
+            //若模型沒通過驗證則顯示目前的view
+            if(ModelState.IsValid == false)
+            {
+                return View();
+            }
+            //依帳號取得會員並指定給member
+            var member = db.tMember.Where(m => m.fUserId == pMember.fUserId).FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if(member == null)
+            {
+                //將會員記錄新增到tMember資料表
+                db.tMember.Add(pMember);
+                db.SaveChanges();
+                //執行home控制器的login動作方法
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "此帳號已有人使用，註冊失敗";
+            return View();
+        }
+    }
+}
---
+++ slnShoppingCa/Controllers/MemberController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using slnShoppingCa.Models;
+
+namespace slnShoppingCa.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        dbShoppingCarEntities1 db = new dbShoppingCarEntities1();
+        // GET: Member
+        public ActionResult Index()
+        {
+            //取得所有產品放入products
+            var products = db.tProduct.OrderByDescending(m => m.fId).ToList();
+            return View("../Home/Index", "_LayoutMember", products);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
+        //Get:Member/shoppingCar
+        public ActionResult ShoppingCar()
+        {
+            //取的登入會員的帳號並指定給fUserId
+            string fUserId = User.Identity.Name;
+            //找出未成為訂單明細的資料，即購物車內容
+            var orderDetails = db.tOrderDetail.Where(m => m.fUserId == fUserId && m.fIsApproved == "否").ToList();
+            //view使用orderdetails
+            return View(orderDetails);
+        }
+
+        //Post:Member/shoppingcar
+        [HttpPost]
+        public ActionResult ShoppingCar(string fReceiver, string fEmail, string fAddress)
+        {
+            string fUserId = User.Identity.Name;
+            string guid = Guid.NewGuid().ToString();
+
+            tOrder order = new tOrder();
+            order.fOrderGuid = guid;
+            order.fUserId = fUserId;
+            order.fReceiver = fReceiver;
+            order.fEmail = fEmail;
+            order.fAddress = fAddress;
+            order.fDate = DateTime.Now;
+            db.tOrder.Add(order);
+
+            var carList = db.tOrderDetail.Where(m => m.fIsApproved == "否" && m.fUserId == fUserId).ToList();
+
+            foreach(var item in carList)
+            {
+                item.fOrderGuid = guid;
+                item.fIsApproved = "是";
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("OrderList");
+        }
+
+        public ActionResult AddCar(string fPId)
+        {
+            //取得會員帳號並指定給fUserId
+            string fUserId = User.Identity.Name;
+            //找出會員放入訂單明細的產品，該產品的fisapproved為"否"
+            //表示該產品是購物車狀態
+            var currentCar = db.tOrderDetail.Where(m => m.fPId == fPId && m.fIsApproved == "否" && m.fUserId == fUserId).FirstOrDefault();
+            //若currentCar等於null，表示會員選購的產品不是購物車狀態
+            if(currentCar == null)
+            {
+                //找出目前選購的產品並指定給product
+                var product = db.tProduct.Where(m => m.fPId == fPId).FirstOrDefault();
+                //將產品放入訂單明細，因為產品的fIsAPPROVED為"否"，表示為購物車狀態
+                tOrderDetail orderDetail = new tOrderDetail();
+                orderDetail.fUserId = fUserId;
+                orderDetail.fPId = product.fPId;
+                orderDetail.fName = product.fName;
+                orderDetail.fPrice = product.fPrice;
+                orderDetail.fQty = 1;
+                orderDetail.fIsApproved = "否";
+                db.tOrderDetail.Add(orderDetail);
+            }
+            else
+            {
+                //若產品為購物車狀態,，即將該產品數量加1
+                currentCar.fQty += 1;
+            }
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        public ActionResult DeleteCar(int fId)
+        {
+            //依fid找出要刪除購物車狀態的產品
+            var orderDetail = db.tOrderDetail.Where(m => m.fId == fId).FirstOrDefault();
+            //刪除購物車狀態商品
+            db.tOrderDetail.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        public ActionResult OrderList()
+        {
+            string fUserId = User.Identity.Name;
+            var orders = db.tOrder.Where(m => m.fUserId == fUserId).OrderByDescending(m => m.fDate).ToList();
+            return View(orders);
+        }
+
+        public ActionResult OrderDetail( string fOrderGuid)
+        {
+            var orderDetails = db.tOrderDetail.Where(m => m.fOrderGuid == fOrderGuid).ToList();
+            return View(orderDetails);
+        }
+    }
+}
